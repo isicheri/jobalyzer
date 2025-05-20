8fef3a080013d06f5476ca4f152c3f28a7fb762b
isicheri <tech2dom@gmail.com>
2025-05-20 23:40:52 +0100

    still setting up the project

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log(process.cwd());
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+app_1.default.listen(8000, () => {
+    console.log("server is running..");
+});
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import helmet from "helmet";
 import path from "path";
+import session from "express-session";
+import pgSession from "connect-pg-simple";
+import { Pool } from "pg";
+import limiter from "./config/ratelimiter/ratelimiter";
 const App = express();
 
 App.use(helmet())
+App.use(limiter)
+App.use(express.urlencoded({ extended: true }));
 App.set("view engine","ejs")
 App.use(express.static(path.join(process.cwd(),"public")))
 App.get("/",(req,res) => {
     res.sendFile("index.html")
 })
-
 
 export default App;
